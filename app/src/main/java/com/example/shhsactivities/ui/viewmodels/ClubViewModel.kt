626efbe3cfc161b84c982c160a4988640ad4331e
CruidGals <kchiem30@gmail.com>
2024-02-05 17:35:12 -0500

    Add Club Screen Events and boilerplate code

--- app/src/main/java/com/example/shhsactivities/ui/viewmodels/ClubViewModel.kt
+++ app/src/main/java/com/example/shhsactivities/ui/viewmodels/ClubViewModel.kt
@@ -1,11 +1,15 @@
-package com.example.shhsactivities.ui.theme.viewmodels
+package com.example.shhsactivities.ui.viewmodels
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.shhsactivities.data.Announcement
 import com.example.shhsactivities.data.Club
+import com.example.shhsactivities.ui.events.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,6 +17,9 @@ import javax.inject.Inject
 class ClubViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ): ViewModel() {
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
     var club = mutableStateOf<Club?>(null)
 
     init {
@@ -22,8 +29,17 @@ class ClubViewModel @Inject constructor(
             }
         }
     }
+
+    private fun sendUiEvent(event: UiEvent){
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
 }
 
 sealed class ClubScreenEvent {
-
+    data object OnClubPictureClick: ClubScreenEvent()
+    data object OnGoalClick: ClubScreenEvent()
+    data object OnRosterClick: ClubScreenEvent()
+    data class OnAnnouncementClick(val announcement: Announcement): ClubScreenEvent()
 }
