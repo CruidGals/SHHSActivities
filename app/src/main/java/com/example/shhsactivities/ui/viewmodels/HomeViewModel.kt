d3e8937d1e8cf8bcfb1ede2346268592c2d55d8d
CruidGals <kchiem30@gmail.com>
2024-02-08 18:52:51 -0500

    Setup query clubs by Order function

--- app/src/main/java/com/example/shhsactivities/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shhsactivities/ui/viewmodels/HomeViewModel.kt
@@ -4,8 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.shhsactivities.data.Club
 import com.example.shhsactivities.ui.states.ClubsRetrievalState
+import com.example.shhsactivities.ui.viewmodels.components.ClubOrder
+import com.example.shhsactivities.ui.viewmodels.components.OrderDirection
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class HomeViewModel: ViewModel() {
@@ -17,8 +23,26 @@ class HomeViewModel: ViewModel() {
     var homeUIState by mutableStateOf(HomeUIState())
         private set
 
-    fun queryClubsByOrder() {
-
+    fun queryClubsByOrder(order: ClubOrder) {
+        viewModelScope.launch {
+            val clubs: Flow<List<Club>> = flow{} //TODO Repository
+            clubs.map { clubs ->
+                when(order.direction) {
+                    is OrderDirection.Ascending -> {
+                        when(order) {
+                            is ClubOrder.Leadership -> TODO()
+                            is ClubOrder.Title -> TODO()
+                        }
+                    }
+                    is OrderDirection.Descending -> {
+                        when(order) {
+                            is ClubOrder.Leadership -> TODO()
+                            is ClubOrder.Title -> TODO()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
