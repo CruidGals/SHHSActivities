b3157b88b76a5de326fc1c8d4b79a05a7424b5ef
CruidGals <kchiem30@gmail.com>
2024-03-18 23:47:44 -0400

    Simplify Catalog Screen

--- app/src/main/java/com/example/shhsactivities/ui/screens/CatalogScreen.kt
+++ app/src/main/java/com/example/shhsactivities/ui/screens/CatalogScreen.kt
@@ -13,10 +13,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,71 +54,12 @@ fun CatalogScreen(
             .background(Color.White)
     ) {
         val listState = rememberLazyListState()
-        val isFirstVisible = remember {
-            derivedStateOf { listState.firstVisibleItemIndex > 0 }
-        }
 
         LazyColumn(
             state = listState,
             modifier = Modifier.fillMaxSize()
         ) {
             stickyHeader {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.Gray)
-                        .padding(3.dp)
-                ) {
-                    AnimatedContent(
-                        targetState = isFirstVisible.value, label = "Catalog"
-                    ) { isFirstVisible ->
-                        if (isFirstVisible) {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(start = 5.dp)
-                            ) {
-                                Text(
-                                    modifier = Modifier.align(Alignment.CenterStart),
-                                    text = "Catalog",
-                                    style = Typography.headlineSmall,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.White
-                                )
-
-                                /*IconButton(
-                                    modifier = Modifier.align(Alignment.CenterEnd),
-                                    onClick = { onSearchClick() }
-                                ) {
-                                    Icon(
-                                        Icons.Default.Search,
-                                        contentDescription = Icons.Default.Search.name,
-                                        tint = Color.White
-                                    )
-                                }*/
-                            }
-                        } else {
-                            Column(
-                                modifier = Modifier.padding(
-                                    top = 30.dp,
-                                    start = 5.dp,
-                                    bottom = 5.dp
-                                ),
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                Text(
-                                    text = "Catalog",
-                                    style = Typography.headlineLarge,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.White
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-
-            item {
                 SearchBar(
                     query = searchQuery,
                     onQueryTextChange = { viewModel.editSearchQuery(it)},
