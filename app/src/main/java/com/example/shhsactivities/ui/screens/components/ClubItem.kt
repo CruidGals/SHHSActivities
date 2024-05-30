209becb4a46bd80f19b09d53cd60b878c157f6df
CruidGals <kchiem30@gmail.com>
2024-05-30 00:37:27 -0400

    Implement auto-resizing text

--- app/src/main/java/com/example/shhsactivities/ui/screens/components/ClubItem.kt
+++ app/src/main/java/com/example/shhsactivities/ui/screens/components/ClubItem.kt
@@ -1,5 +1,6 @@
 package com.example.shhsactivities.ui.screens.components
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,15 +16,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -122,15 +130,31 @@ fun MiniClubItem(
         ){
             Column (
                 modifier = Modifier
-                    .weight(0.60f)
-                    .padding(horizontal = 8.dp)
+                    .weight(0.70f)
+                    .padding(start = 8.dp)
                     .fillMaxHeight(),
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
+                var textStyle by remember { mutableStateOf(Typography.headlineSmall) }
+                var readyToDraw by remember { mutableStateOf(false) }
+
                 Text(
                     text = club.name,
-                    style = Typography.headlineSmall,
-                    fontWeight = FontWeight.Bold
+                    style = textStyle,
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Clip,
+                    maxLines = 1,
+                    softWrap = false,
+                    modifier = modifier.drawWithContent {
+                        if (readyToDraw) drawContent()
+                    },
+                    onTextLayout = { textLayoutResult ->
+                        if (textLayoutResult.didOverflowWidth) {
+                            textStyle = textStyle.copy(fontSize = textStyle.fontSize * 0.9)
+                        } else {
+                            readyToDraw = true
+                        }
+                    }
                 )
                 Text(
                     text = "Room ${club.room}",
@@ -140,7 +164,7 @@ fun MiniClubItem(
             }
 
             Column(
-                modifier = Modifier.fillMaxSize().weight(.40f),
+                modifier = Modifier.fillMaxSize().weight(.30f),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
